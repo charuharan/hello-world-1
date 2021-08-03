d76d32b3f94186712868850d56458de65e97bb65
charuharan <chari301431@gmail.com>
2021-08-03 21:24:50 +0530

    i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,16 @@ import { Component, ViewEncapsulation,Input } from '@angular/core';
 })
 export class AppComponent { 
   n1="interaction";
+  minutes = 0;
+  gender = 'female';
+  fly = true;
+  logo = 'https://angular.io/assets/images/logos/angular/angular.png';
+  inc(i: number) {
+    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
+  }
+  male() { this.gender = 'male'; }
+  female() { this.gender = 'female'; }
+  other() { this.gender = 'other'; }
 }
+
 
